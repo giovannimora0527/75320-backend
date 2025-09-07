6bb573e87daee086831df0f58c520df89d58265d
Adriana Castro <maryerifer2002@gmail.com>
2025-09-07 18:11:05 -0500

    Se agraga listado de pacientes y busqueda de documento

---
+++ src/main/java/com/uniminuto/clinica/api/PacienteApi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.uniminuto.clinica.api;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import com.uniminuto.clinica.entity.Paciente;
+
+@RequestMapping("/paciente")
+public interface PacienteApi {
+
+    @GetMapping("/EncontrarPacientes")
+    ResponseEntity<List<Paciente>> listarPacientes();
+
+    @GetMapping("/usuarios/{documento}")
+    ResponseEntity<Paciente> buscarPorDocumento(@PathVariable String documento);
+}
+
---
+++ src/main/java/com/uniminuto/clinica/service/PacienteService.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.uniminuto.clinica.service;
+import java.util.List;
+import com.uniminuto.clinica.entity.Paciente;
+
+
+public interface PacienteService {
+    List<Paciente> listarPacientes();
+    Paciente buscarPorDocumento(String numeroDocumento);
+}
+
