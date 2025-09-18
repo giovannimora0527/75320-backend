3ca276d1f1cb4b12bf7f0b75533d7b8605f82ad5
MaicolDa <tecnicolaboraldavid@gmail.com>
2025-09-17 22:41:08 -0500

    Actualizo servicios y entidades (paciente, cita, receta, etc.)

---
+++ src/main/java/com/uniminuto/clinica/service/impl/PacienteServiceImpl.java
@@ -0,0 +1,38 @@
+package com.uniminuto.clinica.service.impl;
+
+import com.uniminuto.clinica.entity.Paciente;
+import com.uniminuto.clinica.repository.PacienteRepository;
+import com.uniminuto.clinica.service.PacienteService;
+import java.util.List;
+import java.util.Optional;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author lmora
+ */
+@Service
+public class PacienteServiceImpl implements PacienteService {
+
+    @Autowired
+    private PacienteRepository PacienteRepository;
+
+    @Override
+    public List<Paciente> encontrarTodosLosPacientes() {
+        return this.PacienteRepository.findAll();
+    }
+
+    @Override
+    public Paciente buscarPacientePorDocumento(String documento) throws BadRequestException {
+        
+        Optional<Paciente> optPaciente = this.PacienteRepository.findByNumeroDocumento(documento);
+        if (!optPaciente.isPresent()) {
+            throw new BadRequestException("No se encuentra el paciente");
+        
+        }
+        return optPaciente.get();
+    }
+
+}
